ecb5ebe02003167f0b95bc6a22784d4215adbe6a
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-04-26 00:11:04 +0100

    add services

--- GE.Services/IServiceEquipe.cs
+++ GE.Services/IServiceEquipe.cs
@@ -8,7 +8,7 @@ namespace Service
 {
  public   interface IServiceEquipe:IService<Equipe>
     {
-        //double Recompense(Equipe e);
+        double Recompense(Equipe e);
         IEnumerable<Joueur> JoueursTrophee(Trophee t);
         IEnumerable<Entraineur> EntraineurEquipe(Equipe e);
         DateTime DatePremierChampionnat(Equipe e);
